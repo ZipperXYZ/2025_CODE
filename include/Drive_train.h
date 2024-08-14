7e910fe297c488e65456fe6c8f330880faf490a6
ZipperXYZ <gbosse2008@gmail.com>
2024-08-14 06:14:32 +0100

    second commut 2024/08/14

--- include/Drive_train.h
+++ include/Drive_train.h
@@ -1,10 +1,18 @@
+#include "MiniPID.h"
+
 using namespace vex;
 
 struct Drive_train
 {
+    Drive_train(double p,double i,double d,double pt,double it,double dt) : DrivePID(p,i,d), TurnPID(pt,it,dt) {}; // "p" = le p de la pid pour avancer. "pt" = le p de la pid pour tourner
+    Drive_train() = default;
     void Init();
     int movefor();
-    /* data */
+
+
+    private:
+        MiniPID DrivePID;
+        MiniPID TurnPID;
 };
 
 
--- src/Drive_train.cpp
+++ src/Drive_train.cpp
@@ -1,8 +1,15 @@
 #include "vex.h"
 
 #include "Drive_train.h"
+#include "MiniPID.h"
 
 using namespace vex;
+
+Drive_train::Drive_train(double p, double i, double d, double pt, double it, double dt) : DrivePID(p,i,d), TurnPID(pt,it,dt)
+{
+    DrivePID.setOutputLimits(-12,12);
+    TurnPID.setOutputLimits(-12,12);
+}
 
 void Drive_train::Init()
 {
