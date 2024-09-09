e61c1097a4bc13f89edda7413bbb6523c0c34296
ZipperXYZ <gbosse2008@gmail.com>
2024-09-09 15:36:39 -0400

    new code

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,3 +1,4 @@
+#include "Drive_train.h"
 using namespace vex;
 
 extern brain Brain;
@@ -8,17 +9,18 @@ extern motor ClawMotor;
 extern motor ArmMotor;
 
 extern motor leftMotorA;
-extern motor leftMotorB;
-extern motor_group LeftDriveSmart;
-
 extern motor rightMotorA;
-extern motor rightMotorB;
-extern motor_group RightDriveSmart;
 
-extern gyro TurnGyroSmart;
+extern motor_group LeftDriveSmart;
+extern motor_group RightDriveSmart;
+extern Drive_train DriveX;
+
+extern inertial Inertial1;
+extern inertial Inertial2;
+
 
 /**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * Used to initialize code/tasks/sdevices added using tools in VEXcode Pro.
  * 
  * This should be called at the start of your int main function.
  */
